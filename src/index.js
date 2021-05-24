aeff325fea816e03d0524a4b43e6ce7c7e742653
leshakrikun <lesha.krikun@users.noreply.github.com>
2021-05-25 01:18:43 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 class CountdownTimer {
-  constructor({selector, targetDate, onTick}) {
+  
+  constructor({selector, targetDate}) {
     this.selector = selector;
     this.targetDate = targetDate;
-    this.onTick = onTick;
     this.intervalId = null;
-  }
+    this.daysEl= document.querySelector(`${this.selector} [data-value="days"]`);
+    this.hoursEl= document.querySelector(`${this.selector}  [data-value="hours"]`);
+    this.minsEl= document.querySelector(`${this.selector}  [data-value="mins"]`);
+    this.secsEl= document.querySelector(`${this.selector}  [data-value="secs"]`);
+   }
+
   start() {
    this.intervalId = setInterval(() => {
+    
      const currentTime = Date.now();
      const deltaTime = this.targetDate - currentTime;
      const time = this.getTimeComponents(deltaTime);
      this.onTick(time);
    }, 1000);
   };
-   pad(value) {
+
+  pad(value) {
     return String(value).padStart(2,'0');
   };
-  
    getTimeComponents(time) {
   
     let days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
@@ -32,33 +38,21 @@ class CountdownTimer {
       secs = '00'
    }
       return {days, hours, mins, secs};
-    }
+  }
+  onTick ({days, hours, mins, secs}) {
+      this.daysEl.textContent = `${days}`;
+      this.hoursEl.textContent = `${hours}`;
+      this.minsEl.textContent = `${mins}`;
+      this.secsEl.textContent = `${secs}`;
+     }
 }
-
 window.addEventListener("load", () => {
   countdownTimer.start();
 });
-
-  const countdownTimer = new CountdownTimer({
-    selector: '#timer-1',
-    targetDate: new Date('2021-05-30T01:13'),
-    onTick: updateClockFace,
-  });
-
-const refs = {
-  daysEl: document.querySelector( '#timer-1 [data-value="days"]'),
-  hoursEl: document.querySelector('#timer-1 [data-value="hours"]'),
-  minsEl: document.querySelector('#timer-1 [data-value="mins"]'),
-  secsEl: document.querySelector('#timer-1 [data-value="secs"]'),
-}
-
- function updateClockFace ({days, hours, mins, secs}) {
-  refs.daysEl.textContent = `${days}`;
-  refs.hoursEl.textContent = `${hours}`;
-  refs.minsEl.textContent = `${mins}`;
-  refs.secsEl.textContent = `${secs}`;
- }
-
+const countdownTimer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('2021-05-30T01:13'),
+});
 
 
 
